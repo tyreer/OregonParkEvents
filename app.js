31d0d493ce9b164c6a6749b140562c077d5173f2
Robert Tyree <tyreer@gmail.com>
2017-05-14 19:17:35 +0100

    Added Modernizr to detect input type of date and display a date input if browser supports

--- app.js
+++ app.js
@@ -3,31 +3,37 @@
 document.getElementById("getdata").addEventListener("click", makeAJAXRequest)
 
 function makeAJAXRequest () {
-    
-    var request = new XMLHttpRequest();
-    var url = "//oregonstateparks.org/data/index.cfm/parkEvents?descr=";
-     
+
+    let request = new XMLHttpRequest();
+    let url = "//oregonstateparks.org/data/index.cfm/parkEvents?descr=";
+
     request.addEventListener("load", writeResponse);
-    
+
     request.open("GET", url + document.getElementById("query").value);
     request.send();
 }
 
 function writeResponse() {
-    var response;
-    var htmlString = "";
-    
+    let response;
+    let htmlString = "";
+
     response = JSON.parse(this.responseText);
-    
-    console.log(response);
-    
-    for (var i = 0; i < response.length; i++) {
+
+    for (let i = 0; i < response.length; i++) {
         htmlString += "<h3 style='color: blue'>" + response[i].title + "</h3>";
         htmlString += "<p><b>Start time:</b> " + response[i].event_start + "</p>";
         htmlString += "<p><b>Park name:</b> " + response[i].park_name + "</p>";
         htmlString += "<p><b>Location:</b> " + response[i].event_location + "</p>";
         htmlString += "<p>" + response[i].event_description + "</p>";
     }
-    
+
     document.getElementById("results").innerHTML = htmlString;
+}
+
+if (Modernizr.inputtypes.date) {
+  let calendarDiv = document.createElement("div");
+  calendarDiv.innerHTML="<label>Choose a date <input type='date'></label>";
+  document.getElementById("header").appendChild(calendarDiv);
+} else {
+  console.log('Input type date NOT supported');
 }
